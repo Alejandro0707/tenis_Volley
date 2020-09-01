e86845787ae91ed023afab031c1479c326bec3e6
tomcat <tomcat@ip-172-31-19-198.eu-west-1.compute.internal>
2020-09-01 21:50:57 +0000

    Commit all changes from GenMyModel / Architecture Factory

---
+++ taller1 electiva.sql
@@ -0,0 +1,38 @@
+-- 
+-- Uncomment me if you want :)
+-- CREATE DATABASE taller1 electiva;
+
+CREATE TABLE Persona
+(
+    nombre String NOT NULL,
+    apellido String NOT NULL,
+    fecha de nac date NOT NULL,
+    edad String NOT NULL,
+    sexo String NOT NULL,
+    telefono Integer NOT NULL,
+    direccion String NOT NULL
+);
+
+CREATE TABLE Secretaria
+(
+    id_miembre Integer NOT NULL,
+    tarifa Integer NOT NULL
+);
+
+CREATE TABLE Estado
+(
+    id_estado Integer NOT NULL,
+    Descripcion_Estado Boolean NOT NULL
+);
+
+CREATE TABLE date
+(
+
+);
+
+CREATE TABLE menbresia
+(
+    Id_menbresia Integer NOT NULL,
+    fecha de inicio date NOT NULL
+);
+
